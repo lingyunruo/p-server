2b296805c8eef1201ebabcdc67c509e84c75097b
lingyun <lingyun@yonyou.com>
2019-01-31 15:41:04 +0800

    啦啦啦

--- test/static/index.js
+++ test/static/index.js
@@ -1,24 +1,2 @@
 
-
-let xhr = new XMLHttpRequest();
-
-
-xhr.open('POST', '/nccloud/platform/pub/mergerequest.do', true);
-
-xhr.setRequestHeader('Content-Type', 'application/json');
-
-xhr.onreadystatechange = function() {
-    if(xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.responseText);
-    }
-}
-
-xhr.send(JSON.stringify({
-    busiParamJson: '"[{"rqUrl":"/platform/templet/querypage.do","rqJson":"{\n  \"pagecode\": \"60131041p\",\n  \"appcode\": \"60131041\"\n}","rqCode":"template"},{"rqUrl":"/platform/appregister/queryallbtns.do","rqJson":"{\n  \"pagecode\": \"60131041p\",\n  \"appcode\": \"60131041\"\n}","rqCode":"button"},{"rqUrl":"/platform/appregister/queryappcontext.do","rqJson":"{\n  \"appcode\": \"60131041\"}","rqCode":"context"}]"',
-    sysParamJson: {
-        appcode: '',
-        busiaction: 'null-合并请求',
-        from: '',
-        ts: 1539322472540
-    }
-}));
+$('#div2').html('哈哈哈哈');
